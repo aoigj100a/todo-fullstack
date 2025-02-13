119d6d745783aee2b44985c8d569a58540d2865b
aoigj100a <aoigj100@gmail.com>
2025-02-13 17:30:42 +0800

    feat(backend):add update todo API

--- apps/backend/src/routes/todo.routes.ts
+++ apps/backend/src/routes/todo.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { getTodos, createTodo, deleteTodo } from "../controllers/todo.controllers";
+import { getTodos, createTodo, deleteTodo, updateTodo } from "../controllers/todo.controllers";
 
 const router = Router();
 
 router.get("/", getTodos);
 router.post("/", createTodo);
+router.put("/:id", updateTodo);
 router.delete("/:id", deleteTodo)
 
 export default router;
