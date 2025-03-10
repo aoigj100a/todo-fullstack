efc6f52de62caeb9c8a3467fd18a41feb717cfdd
aoigj100a <aoigj100@gmail.com>
2025-03-10 21:23:21 +0800

    chore(eslint): update sub-projects eslint config

--- apps/frontend/.eslintrc.js
+++ apps/frontend/.eslintrc.js
@@ -2,5 +2,8 @@ module.exports = {
     extends: ['../../.eslintrc.js'],
     parserOptions: {
       project: './tsconfig.json',
+    },
+    rules: {
+      'react/react-in-jsx-scope': 'off'
     }
   };
